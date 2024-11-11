c9b10997aa68ef2e5e2371a6b4829ab3205e9347
Rianhahaha <triandiapriliio@gmail.com>
2024-11-11 20:22:14 +0700

    fixing layout

--- src/components/tentang/tentangCard.jsx
+++ src/components/tentang/tentangCard.jsx
@@ -3,8 +3,8 @@ function TentangCard(props) {
     <div className="flex w-full items-center odd:justify-end my-[15rem] ">
       <div className="w-fit relative group">
         <img className="w-[50rem] group-hover:-translate-y-4 hovered" src={props.card} alt="" />
-        <div className="group-hover:-translate-y-5 delay-75  hovered absolute shrink max-w-[20rem] max-h-[20rem] -[20rem] -top-[9rem] right-9 p-[1rem] bg-white group-hover:bg-blueMain rounded-full border-[1rem] border-blueMain  overflow-hidden">
-          <img className=" w-full object-bottom" src={props.foto} alt="" />
+        <div className="group-hover:-translate-y-5 delay-75  hovered absolute max-w-[15rem] max-h-[15rem] md:max-w-[20rem] md:max-h-[20rem] -top-[7rem] md:-top-[9rem] right-9 p-[1rem] bg-white group-hover:bg-blueMain rounded-full border-[1rem] border-blueMain  overflow-hidden">
+          <img className="w-full object-bottom" src={props.foto} alt="" />
         </div>
       </div>
     </div>
--- src/pages/tentang.jsx
+++ src/pages/tentang.jsx
@@ -8,12 +8,12 @@ function Tentang() {
     <>
       <div className="pb-[2rem] pt-[6rem] min-h-dvh relative bg-blueMain text-white">
         <div className="min-h-dvh pt-[3rem] px-3 md:px-[12rem] ">
-          <div className="-rotate-6 text-[5rem] text-center text-white leading-snug text-shadow">
+          <div className="-rotate-6 text-[3rem] md:text-[5rem] text-center text-white leading-snug text-shadow">
             <p>
               Tentang <br /> Hoax Apocalypse
             </p>
           </div>
-          <div className="mt-[5rem] text-2xl text-center">
+          <div className="mt-[5rem] text-lg md:text-2xl text-center">
           Selamat datang di <span className="text-yellowMain"> "Hoax Apocalypse," </span> sebuah game edukatif yang mengajak Anda untuk berpetualang di dunia yang terancam oleh penyebaran hoaks! Dalam permainan ini,  Mainkan peran sebagai Yuta, seorang pemuda berani dan menjunjung tinggi kejujuran, Yuta memiliki misi untuk menyadarkan warga dari pengaruh informasi palsu yang merusak pemahaman mereka.
           Dengan gameplay yang cepat dan penuh tantangan, Anda akan mengetik kata-kata acak yang muncul di layar dalam waktu terbatas untuk membantu warga yang terjebak dalam gelembung hoaks. Setiap kata yang Anda ketik bukan hanya sekadar tantangan, tetapi juga merupakan langkah untuk meningkatkan literasi digital dan kemampuan berpikir kritis Anda.
           </div>
@@ -30,7 +30,7 @@ function Tentang() {
           <Bg name="Game Programmer" direction="right" />
           <Bg name="Game Programmer" direction="left" />
         </div>
-        <div className="min-h-dvh mt-[3rem] px-3 md:px-[12rem] ">
+        <div className="min-h-dvh mt-[3rem] px-3 lg:px-[12rem] ">
           <div className="-rotate-6 text-[5rem] text-white leading-snug text-shadow">
             <p>
               Profil <br /> Pengembang
